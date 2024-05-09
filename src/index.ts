9b5698e22dd01624505e8241af72b37cd3e5e723
GaspardRuan <1039553124@qq.com>
2024-05-10 00:07:46 +0800

    chore: demo for tree-sitter

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-export const one = 1
-export const two = 2
+/* eslint-disable no-console */
+import Parser from 'tree-sitter';
+import Python from 'tree-sitter-python';
+
+export function parse(code: string): Parser.Tree {
+  const parser = new Parser();
+  parser.setLanguage(Python);
+  const tree = parser.parse(code);
+  return tree;
+}
+
+console.log(parse('print("Hello, World!")\n').rootNode.text);
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,13 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest';
+import { parse } from '../src/index';
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
-  })
-})
+describe('parse', () => {
+  it('parses Python code', () => {
+    const code = 'print("Hello, World!")\n';
+    const tree = parse(code);
+    expect(tree.rootNode.toString()).toBe(
+      '(module (expression_statement (call function: (identifier) arguments: '
+      + '(argument_list (string (string_start) (string_content) (string_end))))))',
+    );
+  });
+});
