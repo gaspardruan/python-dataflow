d8fbc6feec194dedbbc73c34d97d17abb3dfd97e
GaspardRuan <1039553124@qq.com>
2024-05-11 23:08:09 +0800

    feat: SyntaxNode type

---
+++ src/type.ts
@@ -0,0 +1,131 @@
+// compount statement
+export const CLASS_DEFINITION = 'class_definition';
+export const DECORATED_DEFINITION = 'decorated_definition';
+export const FOR_STATEMENT = 'for_statement';
+export const FUNCTION_DEFINITION = 'function_definition';
+export const IF_STATEMENT = 'if_statement';
+export const MATCH_STATEMENT = 'match_statement';
+export const TRY_STATEMENT = 'try_statement';
+export const WHILE_STATEMENT = 'while_statement';
+export const WITH_STATEMENT = 'with_statement';
+
+// simple statement
+export const ASSERT_STATEMENT = 'assert_statement';
+export const BREAK_STATEMENT = 'break_statement';
+export const CONTINUE_STATEMENT = 'continue_statement';
+export const DELETE_STATEMENT = 'delete_statement';
+export const EXEC_STATEMENT = 'exec_statement';
+export const EXPRESSION_STATEMENT = 'expression_statement';
+export const FUTURE_IMPORT_STATEMENT = 'future_import_statement';
+export const GLOBAL_STATEMENT = 'global_statement';
+export const IMPORT_FROM_STATEMENT = 'import_from_statement';
+export const IMPORT_STATEMENT = 'import_statement';
+export const NONLOCAL_STATEMENT = 'nonlocal_statement';
+export const PASS_STATEMENT = 'pass_statement';
+export const PRINT_STATEMENT = 'print_statement';
+export const RAISE_STATEMENT = 'raise_statement';
+export const RETURN_STATEMENT = 'return_statement';
+export const TYPE_ALIAS_STATEMENT = 'type_alias_statement';
+
+// expression
+export const AS_PATTERN = 'as_pattern';
+export const BOOLEAN_OPERATOR = 'boolean_operator';
+export const COMPARISON_OPERATOR = 'comparison_operator';
+export const CONDITIONAL_EXPRESSION = 'conditional_expression';
+export const LAMBDA = 'lambda';
+export const NAMED_EXPRESSION = 'named_expression';
+export const NOT_OPERATOR = 'not_operator';
+export const PRIMARY_EXPRESSION = 'primary_expression';
+
+// parameter
+export const DEFAULT_PARAMETER = 'default_parameter';
+export const DICTIONARY_SPLAT_PATTERN = 'dictionary_splat_pattern';
+export const IDENTIFIER = 'identifier';
+export const KEYWORD_SEPARATOR = 'keyword_separator';
+export const LIST_SPLAT_PATTERN = 'list_splat_pattern';
+export const POSITIONAL_SEPARATOR = 'positional_separator';
+export const TUPLE_PATTERN = 'tuple_pattern';
+export const TYPED_DEFAULT_PARAMETER = 'typed_default_parameter';
+export const TYPED_PARAMETER = 'typed_parameter';
+
+// pattern
+export const ATTRIBUTE = 'attribute';
+export const LIST_PATTERN = 'list_pattern';
+export const SUBSCRIPT = 'subscript';
+
+// primary expression
+export const AWAIT = 'await';
+export const BINARY_OPERATOR = 'binary_operator';
+export const CALL = 'call';
+export const CONCATENATED_STRING = 'concatenated_string';
+export const DICTIONARY = 'dictionary';
+export const DICTIONARY_COMPREHENSION = 'dictionary_comprehension';
+export const ELLIPSIS = 'ellipsis';
+export const FALSE = 'false';
+export const FLOAT = 'float';
+export const GENERATOR_EXPRESSION = 'generator_expression';
+export const INTEGER = 'integer';
+export const LIST = 'list';
+export const LIST_COMPREHENSION = 'list_comprehension';
+export const LIST_SPLAT = 'list_splat';
+export const NONE = 'none';
+export const PARENTHESIZED_EXPRESSION = 'parenthesized_expression';
+export const SET = 'set';
+export const SET_COMPREHENSION = 'set_comprehension';
+export const STRING = 'string';
+export const TRUE = 'true';
+export const TUPLE = 'tuple';
+export const UNARY_OPERATOR = 'unary_operator';
+
+export const ALIASED_IMPORT = 'aliased_import';
+export const ARGUMENT_LIST = 'argument_list';
+export const ASSIGNMENT = 'assignment';
+export const ARGUMENTED_ASSIGNMENT = 'argumented_assignment';
+export const BLOCK = 'block';
+export const CASE_CLAUSE = 'case_clause';
+export const CASE_PATTERN = 'case_pattern';
+export const CHEVRON = 'chevron';
+export const CLASS_PATTERN = 'class_pattern';
+export const COMPLEX_PATTERN = 'complex_pattern';
+export const CONSTRAINED_TYPE = 'constrained_type';
+export const DECORATOR = 'decorator';
+export const DICT_PATTERN = 'dict_pattern';
+export const DICTIONARY_SPLAT = 'dictionary_splat';
+export const DOTTED_NAME = 'dotted_name';
+export const ELIF_CLAUSE = 'elif_clause';
+export const ELSE_CLAUSE = 'else_clause';
+export const EXCEPT_CLAUSE = 'except_clause';
+export const EXCEPT_GROUP_CLAUSE = 'except_group_clause';
+export const EXPRESSION_LIST = 'expression_list';
+export const FINALLY_CLAUSE = 'finally_clause';
+export const FOR_IN_CLAUSE = 'for_in_clause';
+export const FORMAT_EXPRESSION = 'format_expression';
+export const FORMAT_SPECIFIER = 'format_specifier';
+export const GENERIC_TYPE = 'generic_type';
+export const IF_CLAUSE = 'if_clause';
+export const IMPORT_PREFIX = 'import_prefix';
+export const INTERPOLATION = 'interpolation';
+export const KEYWORD_ARGUMENT = 'keyword_argument';
+export const KEYWORD_PATTERN = 'keyword_pattern';
+export const LAMBDA_PARAMETERS = 'lambda_parameters';
+export const MEMBER_TYPE = 'member_type';
+export const MODULE = 'module';
+export const PAIR = 'pair';
+export const PARAMETERS = 'parameters';
+export const PARENTHESIZED_LIST_SPLAT = 'parenthesized_list_splat';
+export const PATTERN_LIST = 'pattern_list';
+export const RELATIVE_IMPORT = 'relative_import';
+export const SLICE = 'slice';
+export const SPLAT_PATTERN = 'splat_pattern';
+export const SPLAT_TYPE = 'splat_type';
+export const STRING_CONTENT = 'string_content';
+export const TYPE = 'type';
+export const TYPE_PARAMETER = 'type_parameter';
+export const UNION_PATTERN = 'union_pattern';
+export const UNION_TYPE = 'union_type';
+export const WILDCARD_IMPORT = 'wildcard_import';
+export const WITH_CLAUSE = 'with_clause';
+export const WITH_ITEM = 'with_item';
+export const YIELD = 'yield';
+
+// unnamed and keywords
