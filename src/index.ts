fd795dc87668c5f2289ea9b2db4b2391160d368c
GaspardRuan <1039553124@qq.com>
2024-06-08 13:33:49 +0800

    feat: add python package spectation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export * from './parser';
-export * from './set';
 export * from './type';
 export * from './interface';
+export * from './set';
+export * from './parser';
 export * from './control-flow';
